c5f80f95e687fbb47c84fc5cdcebc08a88ef24d9
Andrey Okunev <endryondrop@gmail.com>
2020-09-23 12:38:10 +0300

    Functions added

---
+++ functions/ChooseSlides.ts
@@ -0,0 +1,10 @@
+import {Redactor} from "./entities/Redactor";
+import {ListSlides} from "./entities/ListSlides";
+
+export {
+    ChooseSlides
+}
+
+function ChooseSlides(Redactor: Redactor, List<Slide>: ListSlides) {
+    return Redactor;
+}
