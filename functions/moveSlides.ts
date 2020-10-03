6781d5c8fbb1f78110789485a170b47c1cd381a8
Nikita Cheremin <a1za2z307@gmail.com>
2020-10-03 14:27:17 +0300

    Added functions definition

--- functions/moveSlides.ts
+++ functions/moveSlides.ts
@@ -1,10 +1,21 @@
 import {Editor} from '../entities/Editor'
-import {Position} from '../entities/Position'
 
 export {
     moveSlides
 }
 
-function moveSlides(redactor: Editor, listSlides: Array<number>, newPosition: Position): Editor {
-    return redactor
+function moveSlides(editor: Editor, listSlides: Array<number>, slideInsertId: number): Editor {
+    let movedSlides = editor.Presentation.Slides.filter(slide => listSlides.includes(slide.Id));
+    let slidesWithoutMovedSlides = editor.Presentation.Slides
+        .filter(slide => !listSlides.includes(slide.Id));
+    let slideMovePoint = slidesWithoutMovedSlides.findIndex(slide => slide.Id = slideInsertId);
+    let slidesBeforeInsertSlide = slidesWithoutMovedSlides.slice(0, slideMovePoint)
+    let slidesAfterInsertSlide = slidesWithoutMovedSlides.slice(slideMovePoint)
+    return {
+        ...editor,
+        Presentation: {
+            ...editor.Presentation,
+            Slides: slidesBeforeInsertSlide.concat(movedSlides).concat(slidesAfterInsertSlide)
+        }
+    }
 }   
