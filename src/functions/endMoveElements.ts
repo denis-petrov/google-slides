99adc81bc15a20c060c830844c42ea75f4032440
Andrey Okunev <endryondrop@gmail.com>
2020-11-18 19:26:38 +0300

    Fixed a bug of moving one element in the presence of another

--- src/functions/endMoveElements.ts
+++ src/functions/endMoveElements.ts
@@ -15,37 +15,39 @@ export function endMoveElements(isMoveElements: boolean) {
         for (let i = 0; i < selectedElements.length; i++) {
             let elem = selectedElements[i]
             s.elements.map(e => {
-                let topLeftPointX = 0
-                let topLeftPointY = 0
                 if (elem && e.id === elem.id) {
-                    let parent = elem.parentNode as HTMLElement
-                    let X = parent.getAttribute('x')
-                    let Y = parent.getAttribute('y')
-                    if (X !== null && X !== undefined) {
-                        topLeftPointX = parseFloat(X.slice(0, -1))
+                    let topLeftPointX = 0
+                    let topLeftPointY = 0
+                    if (elem && e.id === elem.id) {
+                        let parent = elem.parentNode as HTMLElement
+                        let X = parent.getAttribute('x')
+                        let Y = parent.getAttribute('y')
+                        if (X !== null && X !== undefined) {
+                            topLeftPointX = parseFloat(X.slice(0, -1))
+                        }
+
+                        if (Y !== null && Y !== undefined) {
+                            topLeftPointY = parseFloat(Y.slice(0, -1))
+                        }
                     }
 
-                    if (Y !== null && Y !== undefined) {
-                        topLeftPointY = parseFloat(Y.slice(0, -1))
-                    }
+                    let newPos = new Map()
+                    let newTopLeftPoint = new Map()
+                    let newBottomRightPoint = new Map()
+                    let newCenter = new Map()
+                    let bottomRightPointX = Math.round((Math.round((e.bottomRightPoint.x - e.topLeftPoint.x)*100)/100 + topLeftPointX)*100)/100
+                    let bottomRightPointY = Math.round((Math.round((e.bottomRightPoint.y - e.topLeftPoint.y)*100)/100 + topLeftPointY)*100)/100
+                    newTopLeftPoint.set('x', topLeftPointX)
+                    newTopLeftPoint.set('y', topLeftPointY)
+                    newBottomRightPoint.set('x', bottomRightPointX)
+                    newBottomRightPoint.set('y', bottomRightPointY)
+                    newCenter.set('x', e.center.x)
+                    newCenter.set('y', e.center.y)
+                    newPos.set('newTopLeftPoint', newTopLeftPoint)
+                    newPos.set('newBottomRightPoint', newBottomRightPoint)
+                    newPos.set('newCenter', newCenter)
+                    elements.set(e.id, newPos)
                 }
-
-                let newPos = new Map()
-                let newTopLeftPoint = new Map()
-                let newBottomRightPoint = new Map()
-                let newCenter = new Map()
-                let bottomRightPointX = Math.round((Math.round((e.bottomRightPoint.x - e.topLeftPoint.x)*100)/100 + topLeftPointX)*100)/100
-                let bottomRightPointY = Math.round((Math.round((e.bottomRightPoint.y - e.topLeftPoint.y)*100)/100 + topLeftPointY)*100)/100
-                newTopLeftPoint.set('x', topLeftPointX)
-                newTopLeftPoint.set('y', topLeftPointY)
-                newBottomRightPoint.set('x', bottomRightPointX)
-                newBottomRightPoint.set('y', bottomRightPointY)
-                newCenter.set('x', e.center.x)
-                newCenter.set('y', e.center.y)
-                newPos.set('newTopLeftPoint', newTopLeftPoint)
-                newPos.set('newBottomRightPoint', newBottomRightPoint)
-                newPos.set('newCenter', newCenter)
-                elements.set(e.id, newPos)
             })
         }
 
