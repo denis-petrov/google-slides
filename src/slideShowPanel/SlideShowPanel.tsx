21e5d9e3ad8390627a777e35f9e0bbaa4e6b7a9f
Andrey Okunev <endryondrop@gmail.com>
2020-12-03 17:41:45 +0300

    Fixed slideshow panel style

--- src/slideShowPanel/SlideShowPanel.tsx
+++ src/slideShowPanel/SlideShowPanel.tsx
@@ -5,12 +5,22 @@ import PauseIcon from "@material-ui/icons/Pause"
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight"
 import React from "react"
 import {slideShow} from "./slideShow"
+import {getEditor} from "../stateManager/StateManager";
 
 
 export default function SlideShowPanel() {
+    let editor = getEditor()
+    let slidesNumber = 1
+    editor.presentation.slides.map(s => {
+        if (s.id === editor.selectionSlidesId[0]) {
+            slidesNumber = editor.presentation.slides.indexOf(s) + 1
+        }
+    })
+
     return (
         <div id='slide-mask'>
             <div className='presentation_panel'>
+                <div>Slide {slidesNumber}</div>
                 <KeyboardArrowLeftIcon onClick={(evt) => {
                     showLastSlide(evt)
                 }}/>
@@ -23,7 +33,7 @@ export default function SlideShowPanel() {
                 <KeyboardArrowRightIcon onClick={(evt) => {
                     showNextSlide(evt)
                 }}/>
-                <span onClick={(evt) => {slideShow(evt,true); stopShowPresentation()}}>Close</span>
+                <div onClick={(evt) => {slideShow(evt,true); stopShowPresentation()}}>Close</div>
             </div>
         </div>
     )
